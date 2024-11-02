b2d743dfc1a7c3f74844237860076e9305d544bb
Hemanth-4301 <hemanthachar526@gmail.com>
2024-11-02 16:00:24 +0530

    cross origin changes

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://nie-classtrack.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Allow credentials
+  })
+);
 
 mongoose.connect(process.env.URL);
 
